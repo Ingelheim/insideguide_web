1b4e41b94769e46c1acdcd557724c6d09366483b
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2015-05-02 15:24:36 +0200

    changed port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express  = require('express');
 var _ = require("underscore");
 var app      = express();
 var http = require('http').Server(app);
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,5 +10,5 @@ app.get('*', function(req, res) {
 	res.render('index.jade');
 });
 
-http.listen(8000);
+http.listen(port);
 console.log("App listening on port 8000");
